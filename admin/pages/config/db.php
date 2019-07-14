af60f25360be0dca7573c11ea02150da25d1b62a
Ayman-Hunjul <aymanhun@gmail.com>
2019-07-14 14:48:28 +0300

    Update db.php

--- admin/pages/config/db.php
+++ admin/pages/config/db.php
@@ -1,17 +1,18 @@
 <?php
-
 ob_start(); //Turns on output buffering for redirecting
 session_start(); // Starting session
-
-$db['db_host'] = 'localhost';
-$db['db_user'] = 'root';
-$db['db_pass'] = '';
-$db['db_name'] = 'skyfallv2';
-foreach ($db as $key => $value) { //assoc array
-    define(strtoupper($key), $value);
-}
-$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if (!$con) {
-    echo "Failed to connect: " . mysqli_connect_error($connection);
+if (isset($_COOKIE['db_name'])) {
+    $db['db_host'] = $_COOKIE['db_host'];
+    $db['db_user'] = $_COOKIE['db_user'];
+    $db['db_pass'] = isset($_COOKIE['db_pass']) ? $_COOKIE['db_pass'] : "";
+    $db['db_name'] = $_COOKIE['db_name'];
+    foreach ($db as $key => $value) {
+        define(strtoupper($key), $value);
+    }
+    $con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if (!$con) {
+        die(mysqli_connect_error($con));
+    }
+} else {
+    die("You must initialize the database by <a href='installer.php'>installer</a>!");
 }
