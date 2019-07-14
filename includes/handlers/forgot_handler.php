fcc352c551938b377df9c557d967a852e80a6aed
Ayman-Hunjul <aymanhun@gmail.com>
2019-07-14 14:49:02 +0300

    Update forgot_handler.php

--- includes/handlers/forgot_handler.php
+++ includes/handlers/forgot_handler.php
@@ -1,31 +1,25 @@
-<?php
 
+<?php
 $email = "";
 if (isset($_POST['forgot_pass'])) {
-    $email = validation_input($_POST['email']); 
-    $token = bin2hex(openssl_random_pseudo_bytes(50));
+    $email = validation_input($_POST['email']); //Last name
     if(strtolower($email) == "guest@gmail.com"){
         header("Location: index.php");
         exit;
     }
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-
         $transport = new Swift_SmtpTransport('smtp.gmail.com', 587, 'tls');
-        $transport->setUsername('work000test@gmail.com');
-        $transport->setPassword('32145688+++');
-
+        $transport->setUsername('email@gmail.com');
+        $transport->setPassword('password');
         $mailer = new Swift_Mailer($transport);
-
         $message = new Swift_Message('Password Recovery!');
-        $message->setFrom(['work000test@gmail.com' => 'YourMovies Support Team']);
+        $message->setFrom(['from@gmail.com' => 'SkyFall Support Team']);
         $message->setContentType("text/html");
         $message->setTo($email); //change to localhost
         $message->setBody("<h2>Hello, please click on the following link to reset your password"
                 . "<p>"
                 . "<a href='http://skyfall2019.000webhostapp.com/reset.php?email=$email&token=$token'>Reset my password</a>"
                 . "</p>");
-
         if ($mailer->send($message)) {
             array_push($error_array, "Message has been sent, check your inbox<br>");
         } else {
@@ -35,4 +29,3 @@ if (isset($_POST['forgot_pass'])) {
         array_push($error_array, "Invalid email format<br>");
     }
 }
-
